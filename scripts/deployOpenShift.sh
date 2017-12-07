601e9fc81233b0b8cc182cb0cea5d45195e3b185
Rod Norfor <rod@norfor.net>
2017-12-07 12:59:33 +0000

    Update deployOpenShift.sh
    fix location of network_manager.yml

--- scripts/deployOpenShift.sh
+++ scripts/deployOpenShift.sh
@@ -460,7 +460,7 @@ echo $(date) " - Running network_manager.yml playbook"
 DOMAIN=`domainname -d`
 
 # Setup NetworkManager to manage eth0
-runuser -l $SUDOUSER -c "ansible-playbook openshift-ansible/playbooks/byo/openshift-node/network_manager.yml"
+runuser -l $SUDOUSER -c "ansible-playbook openshift-ansible/playbooks/openshift-node/network_manager.yml"
 
 echo $(date) " - Setting up NetworkManager on eth0"
 # Configure resolv.conf on all hosts through NetworkManager
